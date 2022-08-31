4430d173731715fc8c2d40ba0537b540ee4683d3
paquinho89 <paquinho89@hotmail.com>
2022-08-31 20:21:32 +0200

    railway

--- NovaEra/settings.py
+++ NovaEra/settings.py
@@ -273,4 +273,4 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 # git status (Para ver os arquivos que teñen cambios)
 # git add . (Para añadir os arquivos a carpeta git init e logo subilos)
 # git commit -m "nome_do_commit"
-## git push origin main
+# git push origin main
