1380f7e8fc0d88d5f443b4811d929c715725b70a
paquinho89 <paquinho89@hotmail.com>
2024-01-18 17:39:14 +0100

    arranxos_10

--- NovaEra/settings.py
+++ NovaEra/settings.py
@@ -43,7 +43,7 @@ DEBUG = True
 
 #Eiqui a parte de ter o meu host local (127.0.0.1) engado tamén o host de heroku
 #ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'novaera.up.railway.app', 'novaera.gal', '*.novaera.gal', 'www.novaera.gal']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
