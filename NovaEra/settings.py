a52d416150d36e33053e4f4fb63dc518af01d5e5
paquinho89 <paquinho89@hotmail.com>
2024-02-26 20:49:56 +0100

    arranxo_arquivos

--- NovaEra/settings.py
+++ NovaEra/settings.py
@@ -179,16 +179,16 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'bandadegaitas-novaera'
 #AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-3.amazonaws.com/static'
-AWS_S3_CUSTOM_DOMAIN = 'bandadegaitas-novaera.s3.eu-west-3.amazonaws.com'
+#AWS_S3_CUSTOM_DOMAIN = 'bandadegaitas-novaera.s3.eu-west-3.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = True
 #AWS_DEFAULT_ACL = None
 
 #STATIC FILES // Esto non está facendo nada. O que realmente está indicando a rita dos archivos é o AWS_S3_CUSTOM_DOMAIN
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+STATIC_URL = f'https://bandadegaitas-novaera.s3.eu-west-3.amazonaws.com/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 #MEDIA FILES // Esto non está facendo nada
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media_files/'
+MEDIA_URL = f'https://bandadegaitas-novaera.s3.eu-west-3.amazonaws.com/media_files/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #--------------------------------------------------------------------------
 
