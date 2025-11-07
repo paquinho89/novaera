a998a20ab03e9f061689bb16de59686d28c22194
paquinho89 <37455186+paquinho89@users.noreply.github.com>
2025-11-07 13:24:39 +0100

    Update views.py

--- NovaEra/views.py
+++ NovaEra/views.py
@@ -5,6 +5,13 @@ from artigos.models import artigos
 # def home_page(request):
 #     return render(request, 'pages/home.html')
 
+# novaera/views.py
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+############
+
 def obra_social(request):
   return render(request, 'obra_social.html')
 
